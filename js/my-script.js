8e1f659278c2c08c542bc21dfdf24bef4f553e20
gurdeepsingh2000 <gurdeepsingh20000329@gmail.com>
2021-06-07 23:08:36 +0200

    validatura codice coupon aggiunto

--- js/my-script.js
+++ js/my-script.js
@@ -1,15 +1,26 @@
-var burger_price = 50;
-var price_calculator = document.getElementsByClassName('calculate-price-btn')[0];
-var final_price = document.getElementById('final-price')
+var burger_price = 50; //PREZZO DI PARTENZA DELL'HAMBURGER
+var price_calculator = document.getElementsByClassName('calculate-price-btn')[0]; //BOTTONE CALCOLA PREZZO FINALE
+var final_price = document.getElementById('final-price')            //PREZZO FINALE
+var discount_coupon = 'COUPON50'                //CODICE DEL COUPON DA INSERIRE
 
 price_calculator.addEventListener('click' , function(){
-    var add_ingredient = document.getElementsByClassName('check-priceup');
+    var add_ingredient = document.getElementsByClassName('check-priceup');  //CHECKBOX DEI INGREDIENTI EXTRA
 
-    for(var x = 0; x < add_ingredient.length; x++){
+    for(var x = 0; x < add_ingredient.length; x++){             //CONTROLLO SE LE CASELLE SONO STATE CLICCATE
         if(add_ingredient[x].checked){
-            burger_price += parseInt(add_ingredient[x].value);
+            burger_price += parseInt(add_ingredient[x].value); //SE CLICCATE AGGIUNGO IL PREZZO DELL'INGREDIENTE CLICCATO A QUELLO FINALE
         }
     }
-    final_price.innerHTML = burger_price
+    final_price.innerHTML = burger_price;
+
+    var codeDiscount = document.getElementById('discount-coupon');          //INPUT DEL CODICE COUPON
+    if(codeDiscount.value === discount_coupon){                     // CONTROLLO SE IL VALORE INSERITO DALL'UTENTE E UGUALE AL CODICE COUPON DEL SISTEMA
+        alert('Il coupon è stato correttamente validato')
+        discounted_price = burger_price * 50 / 100;             // CALCOLO DEL PREZZO SCONTATO (50%)
+        final_price.innerHTML ='Il prezzo finale è ' + discounted_price;
+    }else{
+        alert('Attenzione hai inserito un codice coupon errato!')
+        final_price.innerHTML = 'Il prezzo finale è ' + burger_price
+    }
 
 })
