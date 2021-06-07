76ae7fbafdd3b39f61b22e29d9193a7faa4e305d
gurdeepsingh2000 <gurdeepsingh20000329@gmail.com>
2021-06-07 17:38:53 +0200

    update

--- js/my-script.js
+++ js/my-script.js
@@ -1,11 +1,14 @@
 var burger_price = 50;
-var add_ingredient = document.getElementsByClassName('check-priceup')[0];
+var price_calculator = document.getElementsByClassName('calculate-price-btn')[0];
+var final_price = document.getElementById('final-price')
 
+price_calculator.addEventListener('click' , function(){
+    var add_ingredient = document.getElementsByClassName('check-priceup');
 
-add_ingredient.addEventListener('click', function () {
-    for(var x = 0; x < add_ingredient.length ; x++){
+    for(var x = 0; x < add_ingredient.length; x++){
         if(add_ingredient[x].checked){
-            console.log(add_ingredient + burger_price)
+            burger_price += parseInt(add_ingredient[x].value);
         }
     }
+    final_price.innerHtml = burger_price
 })
